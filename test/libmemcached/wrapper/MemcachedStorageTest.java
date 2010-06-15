7d37ce872c7631e00ff6cffc581781790de26fee
Yusuke Hata <nowelium+github@gmail.com>
2010-06-15 11:55:54 +0900

    test: gets cas with buffer_requests behavior

--- test/libmemcached/wrapper/MemcachedStorageTest.java
+++ test/libmemcached/wrapper/MemcachedStorageTest.java
@@ -1,6 +1,13 @@
 package libmemcached.wrapper;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import libmemcached.exception.LibMemcachedException;
 import libmemcached.wrapper.type.BehaviorType;
@@ -9,6 +16,7 @@ import libmemcached.wrapper.type.ReturnType;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class MemcachedStorageTest {
@@ -141,4 +149,210 @@ public class MemcachedStorageTest {
         Assert.assertTrue(-1 < result.getCAS());
     }
 
+    @Test
+    public void gets_1() throws LibMemcachedException {
+        connectServer();
+        
+        MemcachedStorage storage = client.getStorage();
+        storage.set("key-1", "value-1", 10, 0);
+        
+        {
+            MemcachedResult result = storage.gets("key-1");
+            Assert.assertEquals(result.getKey(), "key-1");
+            Assert.assertEquals(result.getValue(), "value-1");
+            Assert.assertNotNull(result.getCAS());
+        }
+        {
+            MemcachedResult result = storage.gets("key-2");
+            Assert.assertNull(result);
+        }
+    }
+    
+    @Test
+    public void gets_2() throws LibMemcachedException {
+        connectServer();
+        
+        MemcachedStorage storage = client.getStorage();
+        storage.set("key-1", "value-1", 10, 0);
+        storage.set("key-2", "value-2", 10, 0);
+        
+        {
+            MemcachedResult result = storage.gets("key-1");
+            Assert.assertEquals(result.getKey(), "key-1");
+            Assert.assertEquals(result.getValue(), "value-1");
+            Assert.assertNotNull(result.getCAS());
+        }
+        {
+            MemcachedResult result = storage.gets("key-2");
+            Assert.assertEquals(result.getKey(), "key-2");
+            Assert.assertEquals(result.getValue(), "value-2");
+            Assert.assertNotNull(result.getCAS());
+        }
+        {
+            MemcachedResult result = storage.gets("key-3");
+            Assert.assertNull(result);
+        }
+    }
+    
+    @Test
+    public void gets_set() throws LibMemcachedException {
+        connectServer();
+        
+        MemcachedStorage storage = client.getStorage();
+        storage.gets("test");
+        storage.set("test", "test-value", 10, 0);
+        
+        Assert.assertEquals(storage.get("test"), "test-value");
+    }
+    
+    @Test
+    public void gets_cas_2() throws LibMemcachedException {
+        connectServer();
+        behaviorCas();
+        
+        MemcachedStorage storage = client.getStorage();
+        storage.gets("test");
+        storage.set("test", "test-value", 10, 0);
+        
+        {
+            MemcachedResult result = storage.gets("test");
+            ReturnType rt = storage.cas("test", "test-value2", 10, 0, result.getCAS());
+            if(!ReturnType.SUCCESS.equals(rt)){
+                Assert.fail();
+            }
+        }
+        {
+            MemcachedResult result = storage.gets("test");
+            ReturnType rt = storage.cas("test", "test-value3", 10, 0, result.getCAS());
+            if(!ReturnType.SUCCESS.equals(rt)){
+                Assert.fail();
+            }
+        }
+        Assert.assertEquals(storage.get("test"), "test-value3");
+    }
+    
+    @Test
+    public void cas() throws LibMemcachedException {
+        connectServer();
+        behaviorCas();
+        
+        MemcachedStorage storage = client.getStorage();
+        {
+            ReturnType rt = storage.cas("test", "value-1", 10, 0, 0);
+            // cas key dnt accept: 0
+            Assert.assertEquals(rt, ReturnType.PROTOCOL_ERROR);
+        }
+        {
+            ReturnType rt = storage.cas("test", "value-1", 10, 0, 1);
+            Assert.assertEquals(rt, ReturnType.NOTFOUND);
+        }
+        
+        MemcachedResult result = storage.gets("test");
+        Assert.assertNull(result);
+    }
+    
+    @Test
+    public void cas_MT() throws LibMemcachedException, InterruptedException, ExecutionException {
+        connectServer();
+        behaviorCas();
+        
+        class T implements Callable<String> {
+            MemcachedStorage s;
+            T(MemcachedStorage s){
+                this.s = s;
+            }
+            public String call() throws Exception {
+                int i = 0;
+                while(true){
+                    if(10 < i++){
+                        throw new RuntimeException("max execution");
+                    }
+                    
+                    MemcachedResult r = s.gets("cas-test");
+                    if(r == null){
+                        s.set("cas-test", "0", 10, 0);
+                        return "0";
+                    }
+                    int val = Integer.parseInt(r.getValue());
+                    String valStr = Integer.toString(val + 1);
+                    ReturnType rt = s.cas("cas-test", valStr, 10, 0, r.getCAS());
+                    if(!ReturnType.SUCCESS.equals(rt)){
+                        continue;
+                    }
+                    return valStr;
+                }
+            }
+        }
+        
+        List<String> accepts = new ArrayList<String>();
+        List<Callable<String>> tasks = new ArrayList<Callable<String>>();
+        for(int i = 0; i < 50; i++){
+            tasks.add(new T(client.getStorage()));
+            accepts.add(Integer.toString(i));
+        }
+        
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+        List<Future<String>> futures = exec.invokeAll(tasks);
+        for(Future<String> f: futures){
+            String v = f.get();
+            System.out.println(v);
+            Assert.assertTrue(accepts.contains(v));
+            accepts.remove(v);
+        }
+        Assert.assertTrue(accepts.isEmpty());
+    }
+    
+    @Test
+    @Ignore("CAS does not supported BUFFER_REQUESTS behavior")
+    public void cas_MT_with_behavior_buffer() throws LibMemcachedException, InterruptedException, ExecutionException {
+        connectServer();
+        behaviorCas();
+        client.getBehavior().set(BehaviorType.BUFFER_REQUESTS, true);
+        
+        class T implements Callable<String> {
+            MemcachedStorage s;
+            T(MemcachedStorage s){
+                this.s = s;
+            }
+            public String call() throws Exception {
+                int i = 0;
+                while(true){
+                    if(10 < i++){
+                        throw new RuntimeException("max execution");
+                    }
+                    
+                    MemcachedResult r = s.gets("cas-test");
+                    if(r == null){
+                        s.set("cas-test", "0", 10, 0);
+                        return "0";
+                    }
+                    int val = Integer.parseInt(r.getValue());
+                    String valStr = Integer.toString(val + 1);
+                    ReturnType rt = s.cas("cas-test", valStr, 10, 0, r.getCAS());
+                    if(!ReturnType.SUCCESS.equals(rt)){
+                        continue;
+                    }
+                    return valStr;
+                }
+            }
+        }
+        
+        List<String> accepts = new ArrayList<String>();
+        List<Callable<String>> tasks = new ArrayList<Callable<String>>();
+        for(int i = 0; i < 50; i++){
+            tasks.add(new T(client.getStorage()));
+            accepts.add(Integer.toString(i));
+        }
+        
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+        List<Future<String>> futures = exec.invokeAll(tasks);
+        for(Future<String> f: futures){
+            String v = f.get();
+            System.out.println(v);
+            Assert.assertTrue(accepts.contains(v));
+            accepts.remove(v);
+        }
+        Assert.assertTrue(accepts.isEmpty());
+    }
+    
 }
